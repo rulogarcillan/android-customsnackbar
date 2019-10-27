3565cc10f73f7e193a4b1ae9b8f1a20177881c8d
Raúl Rodríguez Concepción <>
2019-10-27 19:21:44 +0100

    finish

--- app/src/main/java/com/app/myapplication/CustomSnackbar2.kt
+++ app/src/main/java/com/app/myapplication/CustomSnackbar2.kt
@@ -4,96 +4,91 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.BaseTransientBottomBar
+import kotlinx.android.synthetic.main.custom_snackbar_model_two.view.*
 
 
-class CustomSnackbar(
+class CustomSnackbar2(
     parent: ViewGroup,
     content: View,
     contentViewCallback: com.google.android.material.snackbar.ContentViewCallback
-) : BaseTransientBottomBar<CustomSnackbar>(parent, content, contentViewCallback) {
+
+) : BaseTransientBottomBar<CustomSnackbar2>(parent, content, contentViewCallback) {
+
 
 
     init {
         this.getView().setPadding(0, 0, 0, 0)
         this.getView()
             .setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
+
+
     }
 
     /**
      * Sets the title of this custom snackbar.
      */
-    private fun setTitle(title: String): CustomSnackbar {
+    private fun setTitle(title: String): CustomSnackbar2 {
         val titleView = getView().findViewById<TextView>(R.id.tvTitle)
         titleView.text = title
         return this
     }
 
-    /**
-     * Sets the action name of this custom snackbar.
-     */
-    private fun setAction(subtitle: String): CustomSnackbar {
-        val actionButton = getView().findViewById<Button>(R.id.tvAction)
-        actionButton.text = subtitle
-        return this
-    }
-
-    /**
-     * Sets click action  of this custom snackbar.
-     */
-    private fun setClickAction(click: (View) -> Unit): CustomSnackbar {
-        val actionButton = getView().findViewById<Button>(R.id.tvAction)
-        actionButton.setOnClickListener { click(view) }
-        return this
-    }
 
     companion object {
 
         fun make(
             view: View,
             title: Int,
-            action: Int,
-            duration: Int
-        ): CustomSnackbar {
+            duration: Int,
+            gravity: Int = Gravity.BOTTOM
+        ): CustomSnackbar2 {
             return make(
                 view,
                 view.context.getString(title),
-                view.context.getString(action),
-                duration
+                duration,
+                gravity
             )
         }
 
         fun make(
             view: View,
             title: String,
-            action: String,
-            duration: Int
-        ): CustomSnackbar {
+            duration: Int,
+            gravity: Int = Gravity.BOTTOM
+        ): CustomSnackbar2 {
             val customSnackbar = createCustomSnackbar(view).apply {
                 setTitle(title)
-                setAction(action)
                 setDuration(duration)
             }
 
+            val mView = customSnackbar.view
+            val params = mView.layoutParams as FrameLayout.LayoutParams
+            params.gravity = gravity
+            mView.layoutParams = params
+            // customSnackbar.animationMode = ANIMATION_MODE_SLIDE
             return customSnackbar
         }
 
-        private fun createCustomSnackbar(view: View): CustomSnackbar {
+        private fun createCustomSnackbar(view: View): CustomSnackbar2 {
             val parent = findSuitableParent(view) ?: throw IllegalArgumentException(
                 "No suitable parent found from the given view. Please provide a valid view."
             )
 
             val inflater = LayoutInflater.from(view.context)
             val content = inflater.inflate(
-                R.layout.custom_snackbar_model_one,
+                R.layout.custom_snackbar_model_two,
                 parent,
                 false
             )
+
+            content.ivIcon.startAnimation(AnimationUtils.loadAnimation(view.context, R.anim.heart));
+
 
             val contentViewCallback =
                 object : com.google.android.material.snackbar.ContentViewCallback {
@@ -109,7 +104,7 @@ class CustomSnackbar(
                             .setStartDelay(delay.toLong()).start()
                     }
                 }
-            return CustomSnackbar(parent, content, contentViewCallback)
+            return CustomSnackbar2(parent, content, contentViewCallback)
         }
 
 
--- app/src/main/java/com/app/myapplication/MainActivity.kt
+++ app/src/main/java/com/app/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.BaseTransientBottomBar.ANIMATION_MODE_SLIDE
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,28 +15,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun showCustomSnackBarTop(view: View) {
-        showCustomSnackBar(Gravity.TOP)
-    }
 
-    fun showCustomSnackBarBottom(view: View) {
-        showCustomSnackBar(Gravity.BOTTOM)
-    }
 
-    fun showCustomSnackBarCenter(view: View) {
-        showCustomSnackBar(Gravity.CENTER)
-    }
-
-    private fun showCustomSnackBar(gravity: Int) {
+     fun showCustomSnackBar(view: View) {
         CustomSnackbar.make(
             clContainer,
             R.string.title1,
             R.string.action1,
-            Snackbar.LENGTH_LONG,
-            gravity
-        ).show()
+            Snackbar.LENGTH_LONG).show()
+    }
 
-
+     fun showCustomSnackBar2(view: View) {
+        CustomSnackbar2.make(
+            clContainer,
+            getString(R.string.match),
+            Snackbar.LENGTH_LONG).show()
     }
 
 }
